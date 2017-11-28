cf642b845165fdc048db1b9a8f1d2b9b857b70be
Diana <Student@DESKTOP-MI9FM7Q>
2017-11-28 13:29:54 +0100

    Initial GUI

--- IP_Calculator/MainWindow.xaml.cs
+++ IP_Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IP_Calculator.dataVisualization;
 
 namespace IP_Calculator
 {
@@ -20,12 +22,80 @@ namespace IP_Calculator
         public MainWindow()
         {
             InitializeComponent();
-            HelloWorld();
+            createTableAutomaticly();
+            
         }
 
-        private void HelloWorld()
+        private void createTableAutomaticly()
+        {            
+            var dt = new ObservableCollection<dataRow>
+            {
+                new dataRow(){Name = "Address",   Data= "192.168.0.1",         Binary ="11000000.10101000.00000000. 00000001"},
+                new dataRow(){Name = "Netmask",   Data = "255.255.255.0 = 24", Binary ="11111111.11111111.11111111. 00000000"},
+                new dataRow(){Name = "Wildcard",  Data = "0.0.0.255",          Binary ="00000000.00000000.00000000. 11111111"   },
+                new dataRow(){Name = "Network" ,  Data = "192.168.0.0/24",     Binary ="11000000.10101000.00000000. 00000000"  },
+                new dataRow(){Name = "HostMin" ,  Data = "192.168.0.1",        Binary ="11000000.10101000.00000000. 00000001" },
+                new dataRow(){Name = "HostMax" ,  Data = "192.168.0.254",      Binary ="11000000.10101000.00000000. 11111110"  },
+                new dataRow(){Name = "Broadcast", Data = "192.168.0.255",      Binary ="11000000.10101000.00000000. 11111111" },
+                new dataRow(){Name = "Hosts/Net", Data = "254" ,               Binary ="Class C, Private Internet"},
+                new dataRow(){Name = "Host amount", Data = "251" ,             Binary =""},
+                new dataRow(){Name = "Amount of free host", Data = "3" ,       Binary =((5.88)).ToString()+"%"},
+
+            };
+            table.ItemsSource = dt;
+
+            combo.Items.Add("Wi-Fi");
+            combo.Items.Add("Ethernet");
+
+        }
+
+        private void createTableManual()
         {
-            mainLabel.Content = "Hello World!";
+            var dt = new ObservableCollection<dataRow>
+            {
+                new dataRow(){Name = "Netmask",   Data = "255.255.255.0 = 24", Binary ="11111111.11111111.11111111. 00000000"},
+                new dataRow(){Name = "Wildcard",  Data = "0.0.0.255",          Binary ="00000000.00000000.00000000. 11111111"   },
+                new dataRow(){Name = "Network" ,  Data = "192.168.0.0/24",     Binary ="11000000.10101000.00000000. 00000000"  },
+                new dataRow(){Name = "HostMin" ,  Data = "192.168.0.1",        Binary ="11000000.10101000.00000000. 00000001" },
+                new dataRow(){Name = "HostMax" ,  Data = "192.168.0.254",      Binary ="11000000.10101000.00000000. 11111110"  },
+                new dataRow(){Name = "Broadcast", Data = "192.168.0.255",      Binary ="11000000.10101000.00000000. 11111111" },
+                new dataRow(){Name = "Hosts/Net", Data = "254" ,               Binary ="Class C, Private Internet"},
+                new dataRow(){Name = "Amount of free host", Data = "3" ,       Binary =((5.88)).ToString()+"%"},
+
+            };
+            tableManual.ItemsSource = dt;
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void tableManual_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            createTableManual();
         }
     }
 }
---
+++ IP_Calculator/dataVisualization/dataRow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP_Calculator.dataVisualization
+{
+    class dataRow
+    {
+
+        public String Name { get; set; }
+        public String Data { get; set; }
+        public String Binary { get; set; }
+        
+    }
+}
